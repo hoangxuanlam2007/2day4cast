274360f310f969d343a44899713a4eb5d0f18772
Xuan Lam <chim31102007@gmail.com>
2023-11-19 00:28:26 +0700

    Fix css selection from first uppercase letter

--- components/widgets/CurrentWeather.tsx
+++ components/widgets/CurrentWeather.tsx
@@ -12,6 +12,10 @@ interface CurrentWeatherProps {
 export default function CurrentWeather({ data, city }: CurrentWeatherProps) {
   const initial = new Date()
 
+  const capitalizeFirstLetter = (str: string) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+
   return (
     <Card className="relative flex h-fit w-full shrink-0 flex-col justify-between overflow-hidden md:h-[25rem]">
       <div className="absolute " />
@@ -53,7 +57,7 @@ export default function CurrentWeather({ data, city }: CurrentWeatherProps) {
           x={data.sys.pod}
           className="h-9 w-9"
         />
-        <div className="font-semibold weather-description">{data.weather[0].description}</div>
+        <div className="font-semibold weather-description">{capitalizeFirstLetter(data.weather[0].description)}</div>
         <div className="flex gap-2 dark:text-neutral-500">
           <span>Tối đa: {Math.round(data.main.temp_max)}&deg;</span>
           <span>Thấp nhất: {Math.round(data.main.temp_min)}&deg;</span>
